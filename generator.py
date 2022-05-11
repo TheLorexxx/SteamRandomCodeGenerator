0a8062c018d5973c8fdb7105d0c709c05bd5292f
TheLorexxx <104691158+TheLorexxx@users.noreply.github.com>
2022-05-11 20:12:53 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,33 @@
+import time
+import random
+
+steamkod = ["M" , "6" , "8" , "B" , "H" , "Q" , "C" , "2"  , "I" , "A"  , "K" , "Z" , "5" , "0" , "9" , "7" , "3" , "4"  , "P" , "S" , "R" , "T" , "L"]
+
+steamkod1 = random.choice(steamkod)
+steamkod2 = random.choice(steamkod)
+steamkod3 = random.choice(steamkod)
+steamkod4 = random.choice(steamkod)
+steamkod5 = random.choice(steamkod)
+
+steamkodbosluk = "-"
+
+steamkod6 = random.choice(steamkod)
+steamkod7 = random.choice(steamkod)
+steamkod8 = random.choice(steamkod)
+steamkod9 = random.choice(steamkod)
+
+steamkodbosluk2 = "-"
+
+steamkod10 = random.choice(steamkod)
+steamkod11 = random.choice(steamkod)
+steamkod12 = random.choice(steamkod)
+steamkod13 = random.choice(steamkod)
+
+
+print(steamkod1+steamkod2+steamkod3+steamkod4+steamkod5+steamkodbosluk+steamkod6+steamkod7+steamkod8+steamkod9+steamkodbosluk2+steamkod10+steamkod11+steamkod12+steamkod13)
+
+
+time.sleep(99)
+
+
+# Bu program Lorex tarafindan yapilmistir.
